394dac2d5839515dcc05e1403643720ad9c9860d
Joel Alexander <alexandj@etl.boi.rd.hpicorp.net>
2016-05-31 17:01:31 -0600

    add more json parsing

--- site/jsonParse.php
+++ site/jsonParse.php
@@ -1,17 +1,36 @@
 <?php
-require('mysql.php');
-$json = json_decode(file_get_contents('C:/Users/alexandj/WebServer/root/FF_Project/data/2015.json'));
+//require('mysql.php');
+/*
+$json = json_decode(file_get_contents('C:/Users/alexandj/WebServer/root/FF_Project/data/2015/w1.json'));
 //var_dump($json);
 //var_dump($json->players[0]);
 //var_dump($json->players[0]->stats);
-$obj = $json->players[0]->stats;
-foreach($obj as $key => $value){
-    echo "$key : $value ";
+echo"<table><tr><td>ID</td><td>Name</td><td>Position</td><td>team</td></tr>";
+foreach($json->players as $player){
+    echo "<tr><td>$player->id</td><td>$player->name</td><td>$player->position</td><td>$player->teamAbbr</td></tr>";
 }
-echo "<br />";
+echo "</table>";
+*/
+//$json = json_decode(file_get_contents('http://api.fantasy.nfl.com/v1/game/stats?format=json'), true);
+$aContext = array(
+    'http' => array(
+        'proxy' => 'proxy.atlanta.hp.com:8088', // This needs to be the server and the port of the NTLM Authentication Proxy Server.
+        'request_fulluri' => True,
+        ),
+    );
+$cxContext = stream_context_create($aContext);
 
-print_r($obj);
-echo "<br />";
-print $obj->{'1'};
+$url = 'http://api.fantasy.nfl.com/v1/game/stats?format=json';
+$content = file_get_contents($url, False, $cxContext);
+$json = json_decode($content, true);;
+//var_dump($json);
+echo "<table><tr><td>stat id</td><td>abbr</td><td>name</td><td>Short Name</td></tr>";
+foreach ($json["stats"] as $stat){
+    //echo "<tr><td>" . $stat['id'] . "</td><td>" . $stat['abbr'] . "</td><td>" . $stat['name'] . "</td><td>" . $stat['shortname'] . "</td></tr>";
+    echo "<tr><td>" . $stat['id'] . "</td><td>" . $stat['abbr'] . "</td><td>" . $stat['name'] . "</td><td>" . $stat['shortName'] . "</td></tr>";
+    //var_dump($stat);
+}
+
+// ["id"]=> int(1) ["abbr"]=> string(2) "GP" ["name"]=> string(12) "Games Played" ["shortName"]=> string(2) "GP" }
 ?>
 
