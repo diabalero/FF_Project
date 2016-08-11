1dad6cb675149c57cee29315c4b196245c30dd61
Derek <derekcarter614@gmail.com>
2016-08-10 19:32:08 -0600

    Drafthelper.html and script.js updated

--- site/javascript/script.js
+++ site/javascript/script.js
@@ -1,4 +1,13 @@
 $(document).ready(function(){
     $("header").load('menu.html');
     $('#menu').menu();
+
+    /*Slide down the team info for individual teams on the draft helper site*/
+    $('.top').on('click', function() {
+	    $parent_box = $(this).closest('.box');
+	    $parent_box.siblings().find('.bottom').hide();
+	    $parent_box.find('.bottom').toggle();
+    });
+    
+/*final closing bracket*/    
 });
