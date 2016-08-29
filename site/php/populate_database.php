debbcd306a7601295a04df435e4e7ab592699733
Joel Alexander <alexandj@etl.boi.rd.hpicorp.net>
2016-08-29 15:18:09 -0600

    added ignore file which ignore mysql.php

--- site/php/populate_database.php
+++ site/php/populate_database.php
@@ -1,5 +1,6 @@
 <?php
-
+//configure the credentials to your database in mysql.php, the 
+require('../php/mysql.php');
 //function to configure the proxy at HP
 function set_proxy(){
     $aContext = array(
@@ -28,7 +29,7 @@ function add_players_to_database($offset){
             $team = $player['teamAbbr'];
             $gsid = $player['gsisPlayerId'];
             $sql = "INSERT INTO t_players (player_id, name, position, teamAbbr, gsisPlayerId) VALUES ('$player_id', '$name', '$position', '$team', '$gsid')";
-             
+            
         
         }
 }
