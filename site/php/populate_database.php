49fe56153a98b20651740db1f308083ebf0bd7e2
Joel Alexander <alexandj@etl.boi.rd.hpicorp.net>
2016-08-30 14:37:24 -0600

    good stopping point before I branch off

--- site/php/populate_database.php
+++ site/php/populate_database.php
@@ -1,7 +1,7 @@
 <?php
 //configure the credentials to your database in mysql.php 
 require('mysql.php');
-set_time_limit(600);
+set_time_limit(10000);
 //print_r($mysqli);
 
 //function to configure the proxy at HP
@@ -17,11 +17,11 @@ function set_proxy(){
     }
     
 //function to add players to the database
-function add_players_to_database($offset, $mysqli){
+function add_players_to_database($mysqli){
     //set $cxContect to null if you are at home by uncommenting the second line below
     $cxContext = set_proxy();
     //$cxContext = null;
-    
+        for($offset=0;$offset<1000;$offset+=100){
         $url = 'http://api.fantasy.nfl.com/v1/players/editordraftranks?format=json&count=100&offset='.$offset;
         $json = json_decode(file_get_contents($url, true, $cxContext), true);
         $players = $json['players'];
@@ -40,44 +40,44 @@ function add_players_to_database($offset, $mysqli){
             else{
                  printf("Error: %s\n", $mysqli->error);
                 }
+            }
         }    
 }   
 function insert_t_players_stats_from_nfl_api($week, $season, $mysqli){
     //set $cxContect to null if you are at home by uncommenting the second line below
     $cxContext = set_proxy();        
     //$cxContext = null;
-    
-    //here is the URL, I will loop through all the weeks of seasons 2010 to Present
-    $url = "http://api.fantasy.nfl.com/v2/players/weekstats?week=" . $week . "&season=" . $season;
-    //get the stats from the nfl api in json form
-    $json = json_decode(file_get_contents($url, False, $cxContext));
-    //get the game id from the json file, that will be part of the path to the stats
-    $game_id = $json->systemConfig->currentGameId;
-    //create an array of player id's to loop through
-    $player_ids = array();
-    foreach ($json->games->$game_id->players as $key => $value){
-    array_push($player_ids, $key);
-    }
-   //here is the path through the json file for looping reference 
-   //$json->games->$game_id->players->$player_id->stats->week->$season->$week
-    
-    foreach($player_ids as $player_id){
-        foreach($json->games->$game_id->players->$player_id->stats->week->$season->$week as $stat_id =>$value){
-                //execute the mysqli_query statement to insert the data into the db 
-                if($mysqli){
-                        $insert = "INSERT INTO t_player_stats(player_id, stat_id, season, week, value) 
-                        VALUES('" .  $player_id . "', '" . $stat_id . "', '" . $season . "', '" . $week . "', '" . $value . "')";
-                    echo $insert . "<br />";
-                    if($mysqli->query($insert) === TRUE){
-                    echo 'added playerId: '.$player_id.' stat_id: '.$stat_id.' season: '.$season.' week: '.$week.' value: '.$value;
+    for($season = 2010; $season<2016; $season++){
+        for($week=1;$week<18;$week++){
+            //here is the URL, I will loop through all the weeks of seasons 2010 to Present
+        $url = "http://api.fantasy.nfl.com/v2/players/weekstats?week=" . $week . "&season=" . $season;
+        //get the stats from the nfl api in json form
+        $json = json_decode(file_get_contents($url, False, $cxContext));
+        //get the game id from the json file, that will be part of the path to the stats
+        $game_id = $json->systemConfig->currentGameId;
+        //create an array of player id's to loop through
+        $player_ids = array();
+        foreach ($json->games->$game_id->players as $key => $value){
+        array_push($player_ids, $key);
+        }
+        foreach($player_ids as $player_id){
+            foreach($json->games->$game_id->players->$player_id->stats->week->$season->$week as $stat_id =>$value){
+                    //execute the mysqli_query statement to insert the data into the db 
+                    if($mysqli){
+                            $insert = "INSERT INTO t_player_stats(player_id, stat_id, season, week, value) 
+                            VALUES('" .  $player_id . "', '" . $stat_id . "', '" . $season . "', '" . $week . "', '" . $value . "')";
+                        echo $insert . "<br />";
+                        if($mysqli->query($insert) === TRUE){
+                        echo 'added playerId: '.$player_id.' stat_id: '.$stat_id.' season: '.$season.' week: '.$week.' value: '.$value;
+                            }
+                        else{
+                            printf("Error: %s\n", $mysqli->error);
                         }
-                    else{
-                        printf("Error: %s\n", $mysqli->error);
-                    }
-                        }        
-                    }
+                            }        
+                        }
+            }
+        }
     }
-
 }
 
 function insert_t_stats_from_nfl_api($mysqli){
@@ -101,30 +101,73 @@ function insert_t_stats_from_nfl_api($mysqli){
                 printf("Error: %s\n", $mysqli->error);
             }
                 }        
-            }
-    }
+        }
+}
 
+function insert_t_nfl_schedule($mysqli){
+    
+} 
 
-// after creating the database, populate it with this script
+function update_player_table_ranks($mysqli){
+    //set $cxContect to null if you are at home by uncommenting the second line below
+    $cxContext = set_proxy();
+    //$cxContext = null;
+    for($offset = 0;$offset<1000;$offset+=100){
+        $url = 'http://api.fantasy.nfl.com/v1/players/editordraftranks?format=json&count=100&offset='.$offset;
+        $json = json_decode(file_get_contents($url, true, $cxContext), true);
+        $players = $json['players'];
+        foreach($players as $player){
+            $name = mysqli_real_escape_string($mysqli,$player['lastName']);
+            $player_id = mysqli_real_escape_string($mysqli,$player['id']);
+            $rank = mysqli_real_escape_string($mysqli,$player['rank']);
+            $sql = "UPDATE t_players SET rank = '".$rank."' WHERE player_id = '".$player_id."'"; 
+            
+            if($mysqli->query($sql) === TRUE){
+                echo 'updated '. $name . ' set rank to '.$rank.'<br>';
+                }
+            else{
+                 printf("Error: %s\n", $mysqli->error);
+                }
+        }
+    }    
+}
+function update_player_table_adps($mysqli){
+    //set $cxContect to null if you are at home by uncommenting the second line below
+    $cxContext = set_proxy();
+    //$cxContext = null;
+    for($offset = 0;$offset<1000;$offset+=100){
+        $url = 'http://api.fantasy.nfl.com/v1/players/userdraftranks?format=json&count=100&offset='.$offset;
+        $json = json_decode(file_get_contents($url, true, $cxContext), true);
+        $players = $json['players'];
+        foreach($players as $player){
+            $name = mysqli_real_escape_string($mysqli,$player['lastName']);
+            $player_id = mysqli_real_escape_string($mysqli,$player['gsisPlayerId']);
+            $adp = mysqli_real_escape_string($mysqli,$player['rank']);
+            $sql = "UPDATE t_players SET adp = '".$adp."' WHERE gsisPlayerId = '".$player_id."'"; 
+            
+            if($mysqli->query($sql) === TRUE){
+                echo 'updated '. $name . ' set adp to '.$adp.'<br>';
+                }
+            else{
+                 printf("Error: %s\n", $mysqli->error);
+                }
+        }
+    }    
+}
 
-/*
 //this function adds the stats definition to the database, stat_id, stat name etc, no actual player stats 
-insert_t_stats_from_nfl_api($mysqli);
+//insert_t_stats_from_nfl_api($mysqli);
 
 //this function adds the players to the database, player_id, name, position, team, and some other id that might be used for a feed or something
-for($i = 0; $i < 1000; $i+=100){
-    add_players_to_database($i, $mysqli);
-}
+//add_players_to_database($mysqli);
 
 //this function adds the players statistics to the database, weeks 1 thru 17, no playoff stats, though we could add those by changing the week for-loop.
-for ($season = 2010; $season < 2016; $season++){
-    for($week = 1; $week < 18; $week++){
-        insert_t_players_stats_from_nfl_api($week,$season,$mysqli);
-    }
-}
-*/
+//insert_t_players_stats_from_nfl_api($mysqli);
 
-
+//updates the players with current ranks
+//update_player_table_ranks($mysqli);
+//updates the players with current adps
+//update_player_table_adps($mysqli);
 
 
 
