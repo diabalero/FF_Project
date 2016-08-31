f299f1508184d7e39063e3e0a81e14abd910117f
diabalero <derekcarter614@gmail.com>
2016-08-31 14:41:22 -0600

    w3 on menu

--- site/DraftHelper/DH.php
+++ site/DraftHelper/DH.php
@@ -17,21 +17,12 @@
         
         
 
-<<<<<<< HEAD
 
-=======
-<<<<<<< HEAD
-            <div id='menu' class='w3-row'>
-                <div class='menu_div' id='draft_status'></div>
-                <div class='menu_div' id='draft_controls'></div>
-                <div class='menu_div' id='quick_draft_configuration'></div>
-=======
->>>>>>> master
             <div id='menu' class="w3-row">
                 <div class='menu_div w3-col s4 w3-dark-grey' id='draft_status'></div>
                 <div class='menu_div w3-col s4 w3-dark-grey' id='draft_controls'></div>
                 <div class='menu_div w3-col s4 w3-dark-grey' id='quick_draft_configuration'></div>
->>>>>>> TableDesignImprovements
+
             </div>
             <div id="content">
                 <div id='players' class='w3-third w3-left'>
