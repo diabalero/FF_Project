e295a592a66d2926e73018e23514bdd3ba694dc3
Joel Alexander <jt7960@yahoo.com>
2016-08-30 21:38:26 -0600

    did some light housekeeping

--- site/php/playerstats.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$player_ranks = json_decode(file_get_contents("http://api.fantasy.nfl.com/v1/players/editordraftranks?format=json"), true);
-
-print_r($player_ranks['players'][0]);
-//echo "hello";
