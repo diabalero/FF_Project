4b2ae7c8d87259a1cf5b69dc0206b82ab7c6302a
Joel Alexander <jt7960@yahoo.com>
2016-09-20 22:44:50 -0600

    started with some easy stuff tonight

--- index.php
+++ index.php
@@ -1,15 +1,42 @@
 <HTML>
-    <TITLE>Main Page</TITLE>
+    <TITLE>Next Level Fantasy</TITLE>
     <head>
     </head>
-    <?php
-    $json_data = file_get_contents('data/2015.json', 'r');
-    $json_a = json_decode($json_data, true, JSON_FORCE_OBJECT);
-    //var_dump($json_a);
-    var_dump( $json_a['players']);
-    
-    
-    
-    ?>
+    <body>
+        <div id='menu'>
+            <h1>Welcome to Next Level Fantasy</h1>
+        </div>
+        <div id='content'>
+            <h2>What is Next Level Fantasy?</h2>
+                <p>
+                    Next Level Fantasy is a new kind of Fantast Football game. We've added features we love about types of games to fantasy football to make the game
+                    more interactive, more personal, and more strategic. We've taken Fantasy Football to...I won't say it. Here are some features we've added, all of 
+                    which are optional when you set up your league.
+                </p>
+                <p>
+
+            <ul>
+                <li> <a href='link to rules'>Earn upgrade points and level up your roster.</a> For example:
+                    <ul>
+                        <li>Upgrade your QB roster spot from 4 points per passing td, to 6 points.  
+                        <li>Upgrade your RB roster spots from standard scoring, to .5 point ppr, or all the way up to 1pt ppr.</li>
+                        <li>Follow the link above for the full list of upgrades</li>
+                    </ul>
+                <li> <a href='link to rules'>Apply boosters to your roster</a> for point modifyers that highlight your teams strengths or cover its weekness. For Example:
+                    <ul>
+                        <li><b>The RBBC Booster</b>: Own 2 running backs from the same team? Use this booster to play them both!</li>
+                        <li><b>The Weekly Average Booster</b>: Is your stud on bye? Use this booster to take his weekly average instead of playing the waiver wire!</li>
+                        <li><b>The Slinger</b>: Have a pass happy, but turnover prone QB? Use this and he will only score positive points! </li>
+                    </ul>
+                </ul>
+                </p>
+                <p>
+                <h1>Ready to get started?</h1>
+                <a href='link to league creation page'>Click here to create a league.</a> You can change anything later so don't be skerd.
+
+                    
+        </div>
+    </body>
+
 
         
